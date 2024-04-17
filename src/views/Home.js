dfdf61541ead831b7f08d547ef816a8df3749f23
paha <elvispaha@gmail.com>
2024-04-17 10:19:09 +0000

    runtime version

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import {useConfig} from '@dhis2/app-runtime'
 import i18n from '@dhis2/d2-i18n'
+import { Tag } from '@dhis2/ui'
+import React from 'react'
 
-export const Home = () => (
-    <div>
-        <h1>PAHA</h1>
-
-        <p>DHIS2 Web App Academy 2024</p>
+export const Home = () => {
+    const config = useConfig()
+    return(
+        <div>
+         <h1>{i18n.t('Home')}</h1>
+         <p>{i18n.t('DHIS2 Web App Academy 2024')}</p>   
+       
+        <Tag positive>Running on API version {config.apiVersion}</Tag>
+        
     </div>
-)
+    )
+    
+}
